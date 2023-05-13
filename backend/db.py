ab8fb4efc160ca5e67b5c8b79d8b382178b90ae2
Seth Parsons <seth_parsons@ats-engineers.com>
2023-05-13 16:28:05 -0500

    resume data for backend

--- backend/db.py
+++ backend/db.py
@@ -1,18 +1,19 @@
 import json
-import time
-from models import app, db, About
+from models import app, db, AboutTool, BasicInfo, Institution, Workplace, Project, Skill
 
 def populate_db():
     with app.app_context():
         db.drop_all()
         db.create_all()
         populate_about()
+        populate_resume()
 
 def populate_about():
     temp = open("backend_data/about_data.json")
     about_data = json.load(temp)
     temp.close()
 
+    # go through all returned tool results and commit to db
     for tool in about_data["results"]:
         db_row = {
             "name": tool["name"],
@@ -20,10 +21,63 @@ def populate_about():
             "pic": tool["pic"],
             "link": tool["link"],
         }
-        db.session.add(About(**db_row))
-        time.sleep(0.1)
+        db.session.add(AboutTool(**db_row))
     db.session.commit()
 
+def populate_resume():
+    temp = open("backend_data/resume_data.json")
+    resume_data = json.load(temp)
+    temp.close()
+
+    # quick aliases for each resume section
+    # hardcoded, but that's reasonable since I control everything
+    binfo = resume_data["sections"]["Basic Info"]
+    edu = resume_data["sections"]["Education"]
+    proexp = resume_data["sections"]["Professional Experience"]
+    proj = resume_data["sections"]["Projects"]
+    skills = resume_data["sections"]["Key Skills"]
+
+    # Go through all resume sections and commit data for each one
+    basic_info = {
+        "name": binfo["name"],
+        "address": binfo["address"],
+        "email": binfo["email"],
+        "link": binfo["link"],
+        "summary": binfo["summary"],
+    }
+    db.session.add(BasicInfo(**basic_info))
+
+    for result in edu["institutions"]:
+        school = {
+            "name": result["name"],
+            "gpa": result["gpa"],
+            "classes": result["classes"]
+        }
+        db.session.add(Institution(**school))
+    
+    for result in proexp["workplaces"]:
+        work = {
+            "company": result["company"],
+            "title": result["title"],
+            "points": result["points"]
+        }
+        db.session.add(Workplace(**work))
+    
+    for result in proj["projects"]:
+        project = {
+            "name": result["name"],
+            "desc": result["desc"]
+        }
+        db.session.add(Project(**project))
+
+    for result in skills["skills"]:
+        skill = {
+            "text": skill
+        }
+        db.session.add(Skill(**skill))
+
+    # Commit all to database
+    db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import ARRAY
 from flask_cors import CORS
 from dev_secrets import database_link
 
@@ -12,8 +13,38 @@ app.config[
 ] = database_link
 db = SQLAlchemy(app)
 
-class About(db.Model):
+'''
+    Database model for "Tools Used" section of About page.
+'''
+class AboutTool(db.Model):
     name = db.Column(db.String(100), primary_key=True)
     desc = db.Column(db.String(1000))
     pic = db.Column(db.String(1000))
     link = db.Column(db.String(1000))
+
+'''
+    Database models for "Resume" page. Should be self-explanatory.
+'''
+class BasicInfo(db.Model):
+    name = db.Column(db.String(100), primary_key=True)
+    address = db.Column(db.String(1000))
+    email = db.Column(db.String(100))
+    link = db.Column(db.String(1000))
+    summary = db.Column(db.String(1000))
+
+class Institution(db.Model):
+    name = db.Column(db.String(100), primary_key=True)
+    gpa = db.Column(db.String(5))
+    classes = db.Column(db.String(1000))
+
+class Workplace(db.Model):
+    company = db.Column(db.String(100), primary_key=True)
+    title = db.Column(db.String(100))
+    points = db.Column(ARRAY(db.String(1000)))
+
+class Project(db.Model):
+    name = db.Column(db.String(100), primary_key=True)
+    desc = db.Column(db.String(1000))
+
+class Skill(db.Model):
+    text = db.Column(db.String(100), primary_key=True)
